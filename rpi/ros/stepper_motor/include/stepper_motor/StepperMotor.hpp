7c65020b69a84d154e6681a705158b442ffea444
JaeYoung Lee <wayexists92@gmail.com>
2019-05-18 06:48:44 +0000

    I added gate motor

--- rpi/ros/stepper_motor/include/stepper_motor/StepperMotor.hpp
+++ rpi/ros/stepper_motor/include/stepper_motor/StepperMotor.hpp
@@ -35,11 +35,14 @@ private:
 		BOX_RIGHT_MOTOR_CLK, BOX_RIGHT_MOTOR_DIR
 	};
 
-	const MotorInfo support_motor = {
-		SUPPORT_MOTOR_CLK, SUPPORT_MOTOR_DIR
+	const MotorInfo support_motor1 = {
+		SUPPORT_LEFT_MOTOR_CLK, SUPPORT_LEFT_MOTOR_DIR
+	};
+	const MotorInfo support_motor2 = {
+		SUPPORT_RIGHT_MOTOR_CLK, SUPPORT_RIGHT_MOTOR_DIR
 	};
 
-	int initial_motor_clock = 3000; // more less,  more faster
+	int initial_motor_clock = 2000; // more less,  more faster
 	int min_motor_clock = 1000;
 	int motor_speed_up = 10;
 };
